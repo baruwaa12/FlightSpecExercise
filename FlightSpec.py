20ff197424b9802b5224f77248d60c3557958bf0
Ademola <baruwaademola@outlook.com>
2020-11-04 21:11:01 +0000

    Added comments

--- FlightSpec.py
+++ FlightSpec.py
@@ -1,17 +1,18 @@
+# This function validates the flight specification length
 def isValidSpecLength(flightSpec):
     if len(flightSpec) == 12:
         return True
     else:
         return False
 
-
+# Validate first three digits to be either AMS or GLA
 def isFirstThreeDigitsValid(flightSpec):
     firstThree = flightSpec[0:3]
     if firstThree == "AMS" or firstThree == "GLA":
         return True
     return False
 
-
+# Validate fourth digit to be a space
 def isFourthDigitValid(flightSpec):
     fourth = flightSpec[3]
     if fourth == ' ':
@@ -19,7 +20,7 @@ def isFourthDigitValid(flightSpec):
 
     return False
 
-
+# Validation function to check number of bags
 def isFifthDigitValid(flightSpec):
     fifth = flightSpec[4]
     if fifth.isdigit() == False:
@@ -28,7 +29,7 @@ def isFifthDigitValid(flightSpec):
         if int(fifth) <= 3 and int(fifth) >= 0:
             return True
 
-
+# Validate sixth digit to be a space
 def isSixthDigitValid(flightSpec):
     sixth = flightSpec[5]
     if sixth == ' ':
@@ -36,7 +37,7 @@ def isSixthDigitValid(flightSpec):
 
     return False
 
-
+# Validate the age to be between 0 and 99
 def isSeventhDigitValid(flightSpec):
     seventh = flightSpec[6:7]
     if seventh.isdigit() == False:
@@ -45,7 +46,7 @@ def isSeventhDigitValid(flightSpec):
     if int(seventh) <= 99 and int(seventh) >= 0:
         return True
 
-
+# Validate ninth digit to be a space
 def isNinthDigitValid(flightSpec):
     ninth = flightSpec[8]
     if ninth == ' ':
@@ -54,13 +55,14 @@ def isNinthDigitValid(flightSpec):
     return False
 
 
+# Validate the specification to have 1 of 3 meal choices
 def isTenthDigitValid(flightSpec):
     tenth = flightSpec[9]
     if tenth == 'N' or tenth == 'S' or tenth == 'V':
         return True
     return False
 
-
+# Validate eleventh digit to be a space
 def isEleventhDigitValid(flightSpec):
     eleventh = flightSpec[10]
     if eleventh == ' ':
@@ -68,20 +70,27 @@ def isEleventhDigitValid(flightSpec):
 
     return False
 
-
+# Validate that the flight specification has either one of the seating classes
 def isTwelveDigitValid(flightSpec):
     twelveth = flightSpec[11]
     if twelveth == 'E' or twelveth == 'F':
         return True
     return False
 
-
+# Function which validates the whole flight specification
 def isValidSpec(flightSpec):
-    if isValidSpecLength(flightSpec) and isFirstThreeDigitsValid(flightSpec) and isFourthDigitValid(flightSpec) and isFifthDigitValid(flightSpec) and isSixthDigitValid(flightSpec) and isSeventhDigitValid(flightSpec) and isNinthDigitValid(flightSpec) and isTenthDigitValid(flightSpec) and isEleventhDigitValid(flightSpec) and isTwelveDigitValid(flightSpec) == True:
-        return True
-    return False
+    return isValidSpecLength(flightSpec) and \
+        isFourthDigitValid(flightSpec) and \
+        isFifthDigitValid(flightSpec) and \
+        isSixthDigitValid(flightSpec) and \
+        isSeventhDigitValid(flightSpec) and \
+        isNinthDigitValid(flightSpec) and \
+        isTenthDigitValid(flightSpec) and \
+        isEleventhDigitValid(flightSpec) and \
+        isFirstThreeDigitsValid(flightSpec) and \
+        isTwelveDigitValid(flightSpec)
 
-
+# Function to check the flight destination
 def getDestination(flightSpec):
     if flightSpec[0:3] == "GLA":
         return "Glasgow, Scotland"
@@ -89,10 +98,9 @@ def getDestination(flightSpec):
         return "Schiphol, Amsterdam"
     return 'Unknown destination'
 
-
 global FlightCost
 
-
+# Function to check flight cost for either locations
 def getFlightCost(flightSpec):
     if flightSpec[0:3] == "GLA":
         FlightCost = 80.00
@@ -102,7 +110,7 @@ def getFlightCost(flightSpec):
         return FlightCost
     return -1
 
-
+# Function to calculate the baggage costs for a passengers number of bags
 def getBaggageCosts(flightSpec):
     if isFifthDigitValid(flightSpec) == True:
         totalBags = int(flightSpec[4])
@@ -112,7 +120,7 @@ def getBaggageCosts(flightSpec):
 
     return 0.00
 
-
+# Function to calculate the appropriate discounts for a passenger
 def discounts(flightSpec):
 
     if int(flightSpec[6:8]) > 15:
@@ -127,7 +135,7 @@ def discounts(flightSpec):
         TotalDiscount = flightDiscount + MealDiscount
         return TotalDiscount
 
-
+# Function to calculate the meal costs
 def MealCost(flightSpec):
     if flightSpec[9] == 'N':
         return 0
@@ -136,13 +144,14 @@ def MealCost(flightSpec):
     if flightSpec[9] == 'V':
         return 12.00
 
-
+# Function which return the appropriate seating class
 def getSeatingClass(flightSpec):
     if flightSpec[11] == 'E':
         return 'Economy'
     if flightSpec[11] == 'F':
         return 'First Class'
 
+# Function which calculates the cost for a first class passenger or economy passenger
 def totalCost(flightSpec):
     if flightSpec[11] == 'F':
         finalCost = (getFlightCost(flightSpec)*6) + getBaggageCosts(flightSpec) + MealCost(flightSpec) - discounts(flightSpec)
@@ -151,6 +160,7 @@ def totalCost(flightSpec):
         finalCost = getFlightCost(flightSpec) + getBaggageCosts(flightSpec) + MealCost(flightSpec) - discounts(flightSpec)
         return finalCost
 
+# Function to print the flight specfication in a organised format
 def getFlightDetails(flightSpec):
     if isValidSpec(flightSpec) == False:
         print('Invalid Flight Spec')
@@ -163,7 +173,6 @@ def getFlightDetails(flightSpec):
     print('Meal Cost: ' + str(MealCost(flightSpec)))
     print('Seating Class: ' + str(getSeatingClass(flightSpec)))
     print('Total Cost: ' + str(totalCost(flightSpec)))
-
 
 flightSpec = str(input("Please enter a flight specfication? "))
 getFlightDetails(flightSpec)
